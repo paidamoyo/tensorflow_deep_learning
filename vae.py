26f0f8678a3c682da43018d9a104eab2f3b08d02
Paidamoyo <ashgirlchapfuwa@gmail.com>
2017-02-23 12:04:48 -0500

    change  labeled data wighted factor

--- vae.py
+++ vae.py
@@ -379,7 +379,7 @@ def compute_labeled_loss():
     # logpx, logpz, logqz, gv_labeled, gw_labeled
 
     # Reweight gu_labeled and logqy
-    beta = FLAGS['alpha'] * (1. * FLAGS['n_total'] / FLAGS['n_labeled'])
+    beta = FLAGS['alpha'] * FLAGS['n_labeled']
 
     cross_entropy_loss, y_pred_cls = mlp_classifier(z)
     weighted_classification_loss = beta * cross_entropy_loss
@@ -421,7 +421,7 @@ if __name__ == '__main__':
         'encoder_h_dim': 500,
         'decoder_h_dim': 500,
         'latent_dim': 50,
-        'require_improvement': 2000,
+        'require_improvement': 1000,
         'n_total': 50000
     }
 
