0d7b22402c860c1efa68a8eea7b00447fe7109b3
Paidamoyo <ashgirlchapfuwa@gmail.com>
2017-02-25 19:12:04 -0500

    test different concatenation version

--- VAE/semi_supervised/decoder.py
+++ VAE/semi_supervised/decoder.py
@@ -6,47 +6,43 @@ from VAE.utils.tf_helpers import create_h_weights, create_z_weights, activated_n
 
 def generator_network(FLAGS, y_logits, z_latent_rep):
     # Variables
-    w_decoder_h_3, b_decoder_h_3 = create_h_weights('h3', 'decoder',
-                                                    [FLAGS['latent_dim'], FLAGS['decoder_h_dim']])
-    w_decoder_h_4, b_decoder_h_4 = create_h_weights('h4', 'decoder',
-                                                    [FLAGS['decoder_h_dim'], FLAGS['decoder_h_dim']])
-    w_decoder_mu, b_decoder_mu = create_h_weights('mu', 'decoder', [FLAGS['decoder_h_dim'], FLAGS['input_dim']])
+    w_h3, b_h3 = create_h_weights('h3', 'decoder', [FLAGS['latent_dim'], FLAGS['decoder_h_dim']])
+    w_h4, b_h4 = create_h_weights('h4', 'decoder', [FLAGS['decoder_h_dim'], FLAGS['decoder_h_dim']])
+    w_mu, b_mu = create_h_weights('mu', 'decoder', [FLAGS['decoder_h_dim'], FLAGS['input_dim']])
     # Model
     # Decoder hidden layer
-    decoder_h_3 = activated_neuron(pz1_given_z2y(FLAGS=FLAGS, y_logits=y_logits, z_latent_rep=z_latent_rep),
-                                   w_decoder_h_3,
-                                   b_decoder_h_3)
-    decoder_h_4 = activated_neuron(decoder_h_3, w_decoder_h_4, b_decoder_h_4)
+    h3 = activated_neuron(pz1_given_z2y(FLAGS=FLAGS, y_logits=y_logits, z_latent_rep=z_latent_rep), w_h3, b_h3)
+    h4 = activated_neuron(h3, w_h4, b_h4)
 
     # Reconstruction layer
-    x_mu = non_activated_neuron(decoder_h_4, w_decoder_mu, b_decoder_mu)
+    x_mu = non_activated_neuron(h4, w_mu, b_mu)
     tf.summary.image('x_mu', tf.reshape(x_mu[0], [1, 28, 28, 1]))
     return x_mu
 
 
 def pz1_given_z2y(FLAGS, y_logits, z_latent_rep):
-    w_decoder_h_1_z, b_decoder_h_1_z = create_h_weights('h1_z', 'decoder',
-                                                        [FLAGS['latent_dim'], FLAGS['decoder_h_dim']])
-    split_dim = int(FLAGS['decoder_h_dim'] / 2)
-    w_decoder_h_2_z, b_decoder_h_2_z = create_h_weights('h2_z', 'decoder',
-                                                        [FLAGS['decoder_h_dim'], split_dim])
+    w_h1_z, b_h1_z = create_h_weights('h1_z', 'decoder', [FLAGS['latent_dim'], FLAGS['decoder_h_dim']])
 
-    w_decoder_h_1_y, b_decoder_h_1_y = create_h_weights('h1_y', 'decoder',
-                                                        [FLAGS['num_classes'], FLAGS['decoder_h_dim']])
-    w_decoder_h_2_y, b_decoder_h_2_y = create_h_weights('h2_y', 'decoder',
-                                                        [FLAGS['decoder_h_dim'], split_dim])
+    w_h2_z, b_h2_z = create_h_weights('h2_z', 'decoder', [FLAGS['decoder_h_dim'], FLAGS['decoder_h_dim']])
+
+    w_h1_y, b_h1_y = create_h_weights('h1_y', 'decoder', [FLAGS['num_classes'], FLAGS['decoder_h_dim']])
+    w_h2_y, b_h2_y = create_h_weights('h2_y', 'decoder', [FLAGS['decoder_h_dim'], FLAGS['decoder_h_dim']])
+
+    w_h2_mu, b_h2_mu = create_h_weights('h2_mu', 'decoder', [2 * FLAGS['encoder_h_dim'], FLAGS['encoder_h_dim']])
 
     w_mu_z1, w_var_z1, b_mu_z1, b_var_z1 = create_z_weights('z_1_decoder',
                                                             [FLAGS['decoder_h_dim'], FLAGS['latent_dim']])
     # Model
     # Decoder hidden layer
-    decoder_h_1_z = activated_neuron(z_latent_rep, w_decoder_h_1_z, b_decoder_h_1_z)
-    decoder_h_2_z = activated_neuron(decoder_h_1_z, w_decoder_h_2_z, b_decoder_h_2_z)
+    h1_z = activated_neuron(z_latent_rep, w_h1_z, b_h1_z)
+    h2_z = activated_neuron(h1_z, w_h2_z, b_h2_z)
 
-    decoder_h_1_y = activated_neuron(y_logits, w_decoder_h_1_y, b_decoder_h_1_y)
-    decoder_h_2_y = activated_neuron(decoder_h_1_y, w_decoder_h_2_y, b_decoder_h_2_y)
+    h1_y = activated_neuron(y_logits, w_h1_y, b_h1_y)
+    h2_y = activated_neuron(h1_y, w_h2_y, b_h2_y)
+
+    h2_mu = activated_neuron(tf.concat((h2_y, h2_z), axis=1), w_h2_mu, b_h2_mu)
 
     # Z1 latent layer mu and var
-    decoder_logvar_z1 = non_activated_neuron(tf.concat((decoder_h_2_y, decoder_h_2_z), axis=1), w_var_z1, b_var_z1)
-    decoder_mu_z1 = non_activated_neuron(tf.concat((decoder_h_2_y, decoder_h_2_z), axis=1), w_mu_z1, b_mu_z1)
+    decoder_logvar_z1 = non_activated_neuron(h2_z, w_var_z1, b_var_z1)
+    decoder_mu_z1 = non_activated_neuron(h2_mu, w_mu_z1, b_mu_z1)
     return draw_z(FLAGS['latent_dim'], decoder_mu_z1, decoder_logvar_z1)
--- VAE/semi_supervised/encoder.py
+++ VAE/semi_supervised/encoder.py
@@ -6,50 +6,52 @@ from VAE.utils.tf_helpers import create_h_weights, create_z_weights, activated_n
 
 def q_z_1_given_x(FLAGS, x):
     # Variables
-    w_encoder_h_1, b_encoder_h_1 = create_h_weights('h1', 'encoder', [FLAGS['input_dim'], FLAGS['encoder_h_dim']])
-    w_encoder_h_2, b_encoder_h_2 = create_h_weights('h2', 'encoder',
-                                                    [FLAGS['encoder_h_dim'], FLAGS['encoder_h_dim']])
+    w_h1, b_h1 = create_h_weights('h1', 'encoder', [FLAGS['input_dim'], FLAGS['encoder_h_dim']])
+    w_h2, b_h2 = create_h_weights('h2', 'encoder',
+                                  [FLAGS['encoder_h_dim'], FLAGS['encoder_h_dim']])
     w_mu_z1, w_var_z1, b_mu_z1, b_var_z1 = create_z_weights('z_1', [FLAGS['encoder_h_dim'], FLAGS['latent_dim']])
 
     # Hidden layers
-    encoder_h_1 = activated_neuron(x, w_encoder_h_1, b_encoder_h_1)
-    encoder_h_2 = activated_neuron(encoder_h_1, w_encoder_h_2, b_encoder_h_2)
+    h1 = activated_neuron(x, w_h1, b_h1)
+    h2 = activated_neuron(h1, w_h2, b_h2)
 
     # Z1 latent layer mu and var
-    encoder_logvar_z1 = non_activated_neuron(encoder_h_2, w_var_z1, b_var_z1)
-    encoder_mu_z1 = non_activated_neuron(encoder_h_2, w_mu_z1, b_mu_z1)
-    return draw_z(FLAGS['latent_dim'], encoder_mu_z1, encoder_logvar_z1)
+    logvar_z1 = non_activated_neuron(h2, w_var_z1, b_var_z1)
+    mu_z1 = non_activated_neuron(h2, w_mu_z1, b_mu_z1)
+    return draw_z(FLAGS['latent_dim'], mu_z1, logvar_z1)
 
 
 def recognition_network(FLAGS, x):
     # Variables
-    w_encoder_h_3, b_encoder_h_3 = create_h_weights('h3', 'encoder',
-                                                    [FLAGS['latent_dim'], FLAGS['encoder_h_dim']])
-    w_encoder_h_4, b_encoder_h_4 = create_h_weights('h4', 'encoder',
-                                                    [FLAGS['encoder_h_dim'], FLAGS['encoder_h_dim']])
-    w_encoder_h_4_mu, b_encoder_h_4_mu = create_h_weights('h4_mu', 'encoder', [FLAGS['encoder_h_dim'],
-                                                                               FLAGS[
-                                                                                   'encoder_h_dim'] - FLAGS[
-                                                                                   'num_classes']])
+    w_h3_z1, b_h3_z1 = create_h_weights('h3_z1', 'encoder', [FLAGS['latent_dim'], FLAGS['encoder_h_dim']])
+    w_h4_z1, b_h4_z1 = create_h_weights('h4_z1', 'encoder', [FLAGS['encoder_h_dim'], FLAGS['encoder_h_dim']])
+
+    w_h3_y, b_h3_y = create_h_weights('h3_y', 'encoder', [FLAGS['num_classes'], FLAGS['encoder_h_dim']])
+    w_h4_y, b_h4_y = create_h_weights('h4_y', 'encoder', [FLAGS['encoder_h_dim'], FLAGS['encoder_h_dim']])
+
+    w_h4_mu, b_h4_mu = create_h_weights('h4_mu', 'encoder', [2 * FLAGS['encoder_h_dim'], FLAGS['encoder_h_dim']])
 
     w_mu_z2, w_var_z2, b_mu_z2, b_var_z2 = create_z_weights('z_2', [FLAGS['encoder_h_dim'], FLAGS['latent_dim']])
 
     # Model
     z_1 = q_z_1_given_x(FLAGS, x)
+    y_logits = qy_given_x(z_1, FLAGS)
+
     # Hidden layers
-    encoder_h_3 = activated_neuron(z_1, w_encoder_h_3, b_encoder_h_3)
-    encoder_h_4 = activated_neuron(encoder_h_3, w_encoder_h_4, b_encoder_h_4)
-    encoder_h_4_mu = activated_neuron(encoder_h_3, w_encoder_h_4_mu, b_encoder_h_4_mu)
+    h3_z1 = activated_neuron(z_1, w_h3_z1, b_h3_z1)
+    h4_z1 = activated_neuron(h3_z1, w_h4_z1, b_h4_z1)
+
+    h3_y = activated_neuron(y_logits, w_h3_y, b_h3_y)
+    h4_y = activated_neuron(h3_y, w_h4_y, b_h4_y)
+
+    h4_mu = activated_neuron(tf.concat((h4_y, h4_z1), axis=1), w_h4_mu, b_h4_mu)
 
     # Z2 latent layer mu and var
-    y_logits = qy_given_x(z_1, FLAGS)
-    encoder_logvar_z2 = non_activated_neuron(encoder_h_4, w_var_z2, b_var_z2)
-    encoder_mu_z2 = non_activated_neuron(tf.concat((y_logits, encoder_h_4_mu), axis=1), w_mu_z2,
-                                         b_mu_z2)
-    z_2 = draw_z(FLAGS['latent_dim'], encoder_mu_z2, encoder_logvar_z2)
-
+    logvar_z2 = non_activated_neuron(h4_z1, w_var_z2, b_var_z2)
+    mu_z2 = non_activated_neuron(h4_mu, w_mu_z2, b_mu_z2)
+    z_2 = draw_z(FLAGS['latent_dim'], mu_z2, logvar_z2)
     # regularization loss
-    regularization = calculate_regularization_loss(encoder_logvar_z2, encoder_mu_z2)
+    regularization = calculate_regularization_loss(logvar_z2, mu_z2)
 
     return z_2, regularization, y_logits
 
