0896aae0a85c0baa03e69a30dd57aa0480af7e8c
Paidamoyo <ashgirlchapfuwa@gmail.com>
2017-03-02 17:52:45 -0500

    change z prior loss, increase num of iterations

--- VAE/utils/distributions.py
+++ VAE/utils/distributions.py
@@ -9,8 +9,8 @@ def tf_normal_logpdf(x, mu, log_sigma_sq):
     return - 0.5 * logc - log_sigma_sq / 2. - tf.div(tf.squared_difference(x, mu), 2 * tf.exp(log_sigma_sq))
 
 
-def tf_stdnormal_logpdf(x):
-    return - 0.5 * (logc + tf.square(x))
+def tf_stdnormal_logpdf(mu):
+    return - 0.5 * (logc + tf.square(mu))
 
 
 def tf_gaussian_ent(log_sigma_sq):
@@ -40,29 +40,13 @@ def prior_weights():
     return prior_weights_loss
 
 
-# https://github.com/saemundsson/semisupervised_vae/blob/master/vae.py
-
 def compute_ELBO(x_recon, x, y, z):
-    # if self.distributions['p_z'] == 'gaussian_marg'
-    # log_prior_z = tf.reduce_sum(tf_gaussian_marg(z[1], z[2]), 1)
-
-    # elif self.distributions['p_z'] == 'gaussian':
-    log_prior_z = tf.reduce_sum(tf_stdnormal_logpdf(z[0]), 1)
-
-    # if self.distributions['p_y'] == 'uniform':
-
     num_classes = 10
     y_prior = (1. / num_classes) * tf.ones_like(y)
-    log_prior_y = - tf.nn.softmax_cross_entropy_with_logits(logits=y_prior, labels=y)
+    z_prior = tf.ones_like(z[0])  # or z[0]?
+    log_prior_z = tf.reduce_sum(tf_stdnormal_logpdf(mu=z_prior), axis=1)
+    log_prior_y = -tf.nn.softmax_cross_entropy_with_logits(logits=y_prior, labels=y)
+    log_lik = tf.reduce_sum(tf_normal_logpdf(x, x_recon[0], x_recon[1]), axis=1)
+    log_post_z = tf.reduce_sum(tf_normal_logpdf(x=z[0], mu=z[1], log_sigma_sq=z[2]), axis=1)
 
-    # if self.distributions['p_x'] == 'gaussian':
-    log_lik = tf.reduce_sum(tf_normal_logpdf(x, x_recon[0], x_recon[1]), 1)
-
-    # if self.distributions['q_z'] == 'gaussian_marg':
-    # log_post_z = tf.reduce_sum(tf_gaussian_ent(z[2]), 1)
-
-    # elif self.distributions['q_z'] == 'gaussian':
-    log_post_z = tf.reduce_sum(tf_normal_logpdf(z[0], z[1], z[2]), 1)
-    print("log_prior_y:{}, log_lik:{}, log_prior_z:{}, log_post_z:{}".format(log_prior_y.shape, log_lik.shape,
-                                                                             log_prior_z.shape, log_post_z.shape))
     return log_prior_y + log_lik + log_prior_z - log_post_z
