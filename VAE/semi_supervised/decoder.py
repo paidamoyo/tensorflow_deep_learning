1241556761cea5daad664bd25d85e3ea330184da
Paidamoyo <ashgirlchapfuwa@gmail.com>
2017-03-02 13:04:17 -0500

    fix concat dim bug

--- VAE/semi_supervised/decoder.py
+++ VAE/semi_supervised/decoder.py
@@ -25,27 +25,17 @@ def generator_network(FLAGS, y, z, reuse=False):
 
 def pz1_given_z2y(FLAGS, y, z2, reuse=False):
     with tf.variable_scope("decoder", reuse=reuse):
-        w_h1_z, b_h1_z = create_h_weights('h1_z', 'decoder', [FLAGS['latent_dim'], FLAGS['decoder_h_dim']])
-        w_h2_z, b_h2_z = create_h_weights('h2_z', 'decoder', [FLAGS['decoder_h_dim'], FLAGS['decoder_h_dim']])
-
-        w_h1_y, b_h1_y = create_h_weights('h1_y', 'decoder', [FLAGS['num_classes'], FLAGS['decoder_h_dim']])
-        w_h2_y, b_h2_y = create_h_weights('h2_y', 'decoder', [FLAGS['decoder_h_dim'], FLAGS['decoder_h_dim']])
-
-        w_h3, b_h3 = create_h_weights('h2_mu', 'decoder', [2 * FLAGS['encoder_h_dim'], FLAGS['encoder_h_dim']])
+        w_h1, b_h1 = create_h_weights('h1_z', 'decoder',
+                                      [FLAGS['latent_dim'] + FLAGS['num_classes'], FLAGS['decoder_h_dim']])
 
         w_mu_z1, w_var_z1, b_mu_z1, b_var_z1 = create_z_weights('z_1_decoder',
                                                                 [FLAGS['decoder_h_dim'], FLAGS['latent_dim']])
         # Model
         # Decoder hidden layer
-        h1_z = activated_neuron(z2, w_h1_z, b_h1_z)
-        h2_z = activated_neuron(h1_z, w_h2_z, b_h2_z)
-
-        h1_y = activated_neuron(y, w_h1_y, b_h1_y)
-        h2_y = activated_neuron(h1_y, w_h2_y, b_h2_y)
-        print("h2_y:{}, h2_z:{} ".format(h2_y.shape, h2_z.shape))
-        h3 = activated_neuron(tf.concat([h2_y, h2_z], axis=1), w_h3, b_h3)
+        h1 = activated_neuron(tf.concat([y, z2], axis=1), w_h1, b_h1)
+        print("h1 decoder:{}, ".format(h1))
 
         # Z1 latent layer mu and var
-        logvar_z1 = non_activated_neuron(h3, w_var_z1, b_var_z1)
-        mu_z1 = non_activated_neuron(h3, w_mu_z1, b_mu_z1)
+        logvar_z1 = non_activated_neuron(h1, w_var_z1, b_var_z1)
+        mu_z1 = non_activated_neuron(h1, w_mu_z1, b_mu_z1)
         return draw_norm(FLAGS['latent_dim'], mu_z1, logvar_z1)
