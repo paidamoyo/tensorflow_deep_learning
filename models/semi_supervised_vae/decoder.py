69c3f1138188dda566f039bb6d7b67a73e83a33d
Paidamoyo <ashgirlchapfuwa@gmail.com>
2017-03-11 13:43:37 -0500

    fix z1 decoder variable name

--- models/semi_supervised_vae/decoder.py
+++ models/semi_supervised_vae/decoder.py
@@ -7,10 +7,10 @@ from models.utils.tf_helpers import create_nn_weights, mlp_neuron
 def pz1_given_z2y(y, z2, latent_dim, num_classes, hidden_dim, reuse=False):
     with tf.variable_scope("decoder_M2", reuse=reuse):
         # Variables
-        w_h1, b_h1 = create_nn_weights('h1_x', 'decoder', [num_classes + latent_dim, hidden_dim])
-        w_h2, b_h2 = create_nn_weights('h2_x', 'decoder', [hidden_dim, hidden_dim])
+        w_h1, b_h1 = create_nn_weights('h1_z1', 'decoder', [num_classes + latent_dim, hidden_dim])
+        w_h2, b_h2 = create_nn_weights('h2_z1', 'decoder', [hidden_dim, hidden_dim])
 
-        w_mu, b_mu = create_nn_weights('mu_x', 'decoder', [hidden_dim, latent_dim])
+        w_mu, b_mu = create_nn_weights('mu_z1', 'decoder', [hidden_dim, latent_dim])
         w_logvar, b_logvar = create_nn_weights('var_x', 'decoder', [hidden_dim, latent_dim])
 
         h1 = mlp_neuron(tf.concat([y, z2], axis=1), w_h1, b_h1)
