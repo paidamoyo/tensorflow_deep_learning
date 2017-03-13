4d83e4a5dbce7deaadc263c9634dac5f42017ed3
Paidamoyo <ashgirlchapfuwa@gmail.com>
2017-03-13 19:21:53 -0400

    increase num of iterations

--- train_auxiliary.py
+++ train_auxiliary.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     #                    n_minibatches=200, comment='')
 
     FLAGS = {
-        'num_iterations': 3,  # should 3000 epochs (30000/100)
+        'num_iterations': 300000,  # should 3000 epochs (30000/100)
         'num_batches': 100,
         'seed': 31415,
         'n_labeled': 100,
