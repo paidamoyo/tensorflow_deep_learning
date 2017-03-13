55ffbe49fe176f7451e3f3636a04df7711c6d5ae
Paidamoyo <ashgirlchapfuwa@gmail.com>
2017-03-13 16:57:27 -0400

    binarized vae recon image with acc 79per

--- models/auxiliary_semi_supervised/decoder.py
+++ models/auxiliary_semi_supervised/decoder.py
@@ -4,7 +4,7 @@ from models.utils.tf_helpers import create_nn_weights, mlp_neuron
 
 
 def px_given_zy(z, y, hidden_dim, input_dim, latent_dim,  num_classes, reuse=False):
-    with tf.variable_scope("decoder_M1", reuse=reuse):
+    with tf.variable_scope("decoder", reuse=reuse):
         # Variables
         w_h1, b_h1 = create_nn_weights('h1_x', 'decoder', [latent_dim + num_classes, hidden_dim])
         w_h2, b_h2 = create_nn_weights('h2_x', 'decoder', [hidden_dim, hidden_dim])
@@ -19,3 +19,15 @@ def px_given_zy(z, y, hidden_dim, input_dim, latent_dim,  num_classes, reuse=Fal
         x_mu = tf.nn.sigmoid(tf.add(tf.matmul(h2, w_mu), b_mu))
         tf.summary.image('x_mu', tf.reshape(x_mu[0], [1, 28, 28, 1]))
         return x_mu
+
+def pa_given_zyx(z, y, x,  reuse=False):
+    with tf.variable_scope("decoder", reuse=reuse):
+        # Variables
+        w_h1, b_h1 = create_nn_weights('h1_x', 'decoder', [latent_dim + num_classes, hidden_dim])
+        w_h2, b_h2 = create_nn_weights('h2_x', 'decoder', [hidden_dim, hidden_dim])
+
+        w_mu, b_mu = create_nn_weights('mu_x', 'decoder', [hidden_dim, input_dim])
+        # Model
+        # Decoder hidden layer
+        h1 = mlp_neuron(tf.concat((z, y), axis=1), w_h1, b_h1)
+        h2 = mlp_neuron(h1, w_h2, b_h2)
--- models/auxiliary_semi_supervised/encoder.py
+++ models/auxiliary_semi_supervised/encoder.py
@@ -4,17 +4,17 @@ from models.utils.distributions import draw_norm
 from models.utils.tf_helpers import create_nn_weights, mlp_neuron
 
 
-def q_z_given_yx(y, x, latent_dim, num_classes, hidden_dim, input_dim, reuse=False):
-    with tf.variable_scope("encoder_M2", reuse=reuse):
+def q_z_given_ayx(a, y, x, latent_dim, num_classes, hidden_dim, input_dim, reuse=False):
+    with tf.variable_scope("encoder", reuse=reuse):
         # Variables
-        w_h1, b_h1 = create_nn_weights('h1_z', 'encoder', [input_dim + num_classes, hidden_dim])
+        w_h1, b_h1 = create_nn_weights('h1_z', 'encoder', [input_dim + num_classes + latent_dim, hidden_dim])
         w_h2, b_h2 = create_nn_weights('h1_z', 'encoder', [hidden_dim, hidden_dim])
 
         w_mu_z, b_mu_z = create_nn_weights('mu_z', 'encoder', [hidden_dim, latent_dim])
         w_var_z, b_var_z = create_nn_weights('var_z', 'encoder', [hidden_dim, latent_dim])
 
         # Hidden layers
-        h1 = mlp_neuron(tf.concat([y, x], axis=1), w_h1, b_h1)
+        h1 = mlp_neuron(tf.concat([y, x, a], axis=1), w_h1, b_h1)
         h2 = mlp_neuron(h1, w_h2, b_h2)
         # Z2 latent layer mu and var
         logvar_z = mlp_neuron(h2, w_var_z, b_var_z, activation=False)
@@ -24,7 +24,7 @@ def q_z_given_yx(y, x, latent_dim, num_classes, hidden_dim, input_dim, reuse=Fal
 
 
 def q_a_given_x(x, hidden_dim, input_dim, latent_dim, reuse=False):
-    with tf.variable_scope("encoder_M1", reuse=reuse):
+    with tf.variable_scope("encoder", reuse=reuse):
         # Variables
         w_h1, b_h1 = create_nn_weights('h1_a', 'encoder', [input_dim, hidden_dim])
         w_h2, b_h2 = create_nn_weights('h2_a', 'encoder', [hidden_dim, hidden_dim])
