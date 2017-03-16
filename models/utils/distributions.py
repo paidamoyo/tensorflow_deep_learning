2c17c900db0507b6db4a56ad871a14f75fde6b60
Paidamoyo <ashgirlchapfuwa@gmail.com>
2017-03-16 17:32:54 -0400

    update auxiliary loss

--- models/utils/distributions.py
+++ models/utils/distributions.py
@@ -112,11 +112,11 @@ def auxiliary_elbo(x_recon, x, y, qz, qa, pa):
 
     print(x, x_recon)
     log_px = -tf.reduce_sum(tf_binary_xentropy(x_true=x, x_approx=x_recon))
-    logpdf_post_z = tf_normal_logpdf(x=qz[0], mu=qz[1], log_var=qz[2])
+    logpdf_post_z = tf_gaussian_ent(log_sigma_sq=qz[2])
     log_qz = tf.reduce_sum(logpdf_post_z, 1)
     log_qa = tf.reduce_sum(tf_normal_logpdf(x=qa[0], mu=qa[1], log_var=qa[2]))
 
-    logpdf_prior_z = tf_stdnormal_logpdf(x=qa[0])
+    logpdf_prior_z = tf_gaussian_marg(mu=qa[1], log_sigma_sq=qa[2])
     log_pz = tf.reduce_sum(logpdf_prior_z)
     log_py = -tf.nn.softmax_cross_entropy_with_logits(logits=y_prior, labels=y)
     log_pa = tf.reduce_sum(tf_normal_logpdf(x=qa[0], mu=pa[1], log_var=pa[2]))
